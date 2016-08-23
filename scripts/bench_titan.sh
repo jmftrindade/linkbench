091575b1f3c4f628fa7895f89f6a9b31373fd656
Anurag Khandelwal <anuragk@berkeley.edu>
2016-08-23 09:46:15 +0000

    Fixed bench script

--- scripts/bench_titan.sh
+++ scripts/bench_titan.sh
@@ -3,30 +3,18 @@
 sbin="`dirname "$0"`"
 sbin="`cd "$sbin"; pwd`"
 
-echo "Draining Cassandra"
-nodetool drain
-echo "Stopping Cassandra"
-sudo /etc/init.d/cassandra stop
-echo "Deleting previous state and commitlogs"
-sudo rm -rf /raid0/cassandra/commitlog/* /raid0/cassandra/data/small
+function setup() {
+  $sbin/run_on_nodes.sh $sbin/setup.sh
+}
 
-for num_threads in 1 2 4 8 16 32 64 128; do
-  echo "Copying fresh state"
-  sudo cp -r /raid0/cassandra/data/test /raid0/cassandra/data/small
-  echo "Adding user cassanra as user for the new state"
-  sudo chown cassandra:cassandra -R /raid0/cassandra/data/small
-  echo "Starting Cassandra"
-  sudo /etc/init.d/cassandra start
-  echo "Sleeping"
-  sleep 120
-  echo "Enabling thrift"
-  nodetool enablethrift
-  echo "Starting benchmark"
-  $sbin/../bin/linkbench -c $sbin/../config/LinkConfigTitan.properties -r -L $sbin/../titan.t${num_threads}.log -Drequesters=${num_threads}
-  echo "Benchmark finished; draining Cassandra"
-  nodetool drain
-  echo "Stopping Cassandra"
-  sudo /etc/init.d/cassandra stop
-  echo "Removing previous state and commitlogs"
-  sudo rm -rf /raid0/cassandra/commitlog/* /raid0/cassandra/data/small
+echo "Copying benchmark directory"
+$sbin/copy-dir $sbin/../
+
+dataset=$1
+
+for num_threads in 64; do
+  setup $dataset
+  echo "Setup complete"
+  sleep 1
+  $sbin/bench_on_nodes.sh $dataset $num_threads
 done
