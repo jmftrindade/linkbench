9433dbff816eae10a4b0328ed02c23af19e484d3
Anurag Khandelwal <anuragk@berkeley.edu>
2017-02-20 02:51:36 -0800

    Fix

--- bin/stats.cc
+++ bin/stats.cc
@@ -6,59 +6,56 @@
 #include <cstdint>
 #include <unordered_map>
 #include <vector>
+#include <set>
 #include <algorithm>
 
-std::unordered_map<uint64_t, uint32_t> ops;
+std::unordered_map<uint64_t, std::set<uint32_t>> ops;
 std::vector<uint32_t> counts;
 
 uint32_t max = 0;
-double mean = 0.0;
+double sum = 0.0;
 
 void print_usage(const char* exec) {
   fprintf(stderr, "Usage: %s operations-file\n", exec);
 }
 
-void process_ops_file(const std::string& file) {
-  std::ifstream in(file);
+void process_ops_file(std::ifstream& in, size_t op_cnt) {
   uint64_t node_id;
   uint32_t shard_id;
   char sep, type;
   uint64_t num_ops = 0;
-  while (in >> node_id >> sep >> shard_id >> type && sep == ',') {
-    fprintf(stderr, "%llu\t%u\t%c\n", node_id, shard_id, type);
-    ops[node_id]++;
+  while (in >> node_id >> sep >> shard_id >> type && sep == ',' && num_ops <= op_cnt) {
+    ops[node_id].insert(shard_id);
     num_ops++;
   }
-
   fprintf(stderr, "Processed %llu ops; ", num_ops);
-
-  counts.reserve(ops.size());
-  double sum = 0.0;
+  
   for (auto op: ops) {
+    uint32_t count = op.second.size();
     if (op.first < 10000001)
-      counts.push_back(op.second + 1);
+      counts.push_back(count + 1);
     else
-      counts.push_back(op.second);
-    sum += op.second;
-    if (op.second > max)
-      max = op.second;
+      counts.push_back(count);
+    sum += counts.back();
+    if (counts.back() > max)
+      max = counts.back();
   }
-  mean = sum / counts.size();
   std::sort(counts.begin(), counts.end());
 
-  fprintf(stderr, "Max = %u, Mean = %lf\n", max, mean);
+  fprintf(stderr, "Max = %u, Sum = %lf\n", max, sum);
 }
 
-void output_stats(const std::string& file) {
-  std::ofstream mean_out(file + ".mean");
-  mean_out << mean << "\n";
+void output_stats(const std::string& file, uint64_t op_cnt) {
+  std::string out_prefix = file + "." + std::to_string(op_cnt);
+  std::ofstream mean_out(out_prefix + ".mean");
+  mean_out << sum / op_cnt << "\n";
   mean_out.close();
 
-  std::ofstream max_out(file + ".max");
+  std::ofstream max_out(out_prefix + ".max");
   max_out << max << "\n";
   max_out.close();
 
-  std::ofstream count_out(file + ".count");
+  std::ofstream count_out(out_prefix + ".count");
   for (auto count : counts)
     count_out << count << "\n";
   count_out.close();
@@ -71,8 +68,11 @@ int main(int argc, char** argv) {
   }
 
   std::string file = std::string(argv[1]);
-  process_ops_file(file);
-  // output_stats(file);
+  std::ifstream in(file);
+  for (uint64_t i = 1; i <= 10; i++) {
+    process_ops_file(in, 100000000ULL);
+    // output_stats(file, i * 100000000ULL);
+  }
 
   return 0;
 }
